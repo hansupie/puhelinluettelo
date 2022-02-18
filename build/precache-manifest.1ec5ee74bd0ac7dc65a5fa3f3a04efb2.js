4c156b9f084798fe669e7e5d39a57437164c3d1a
Hannemari Kuusisto <hannemari.kuusisto@tuni.fi>
2022-02-18 22:35:23 +0200

    added frontend

---
+++ build/precache-manifest.1ec5ee74bd0ac7dc65a5fa3f3a04efb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32cab89d3cf23cf4021da240c2a2c56d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49bf5b89ff22fc35fea9",
+    "url": "/static/css/main.030145e8.chunk.css"
+  },
+  {
+    "revision": "88b2036836dfb1966812",
+    "url": "/static/js/2.49cef99f.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.49cef99f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49bf5b89ff22fc35fea9",
+    "url": "/static/js/main.4104fd8f.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 
 morgan.token('body', (res) => { return JSON.stringify(res.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
+app.use(express.static('build'))
 
 let persons = [
   {
